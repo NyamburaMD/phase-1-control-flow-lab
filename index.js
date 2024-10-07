e87a17179fac0d27d44de4e4f2af67f715318db6
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2024-10-07 19:38:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-  // change made
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!"
+  }
+  else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) { 
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (true) {
+    case tip >= 20:
+      return "Thank you so much.";
+    case (tip < 20 && tip >= 10)
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
